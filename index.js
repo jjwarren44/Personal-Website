e2be171476b0142f813fb577ca0635d8717d78c5
jjwarren44 <jjwarren44@gmail.com>
2020-02-26 00:25:55 -0500

    first iteration

---
+++ index.js
@@ -0,0 +1,28 @@
+// Function for fading in divs when in view
+(function() {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight < (windowHeight * 0.1) * -1) {
+                element.classList.add('fade-in-element');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
